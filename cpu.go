46deb4dc36d18b9910b6c7e998c71ad8cae97fff
hauptmannhuy <dindago2324@gmail.com>
2025-02-25 21:43:11 +0000

    more instructions

--- cpu.go
+++ cpu.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cpu-emulator/decoder"
 	"log"
+	"math/bits"
 )
 
 type cpu struct {
@@ -79,20 +80,62 @@ func (regs *registers) writeRegs(reg string, b1, b2 uint8) {
 		regs.h = b1
 		regs.l = b2
 	case "SP":
-		regs.sp = make16BitAddr(b1, b2)
+		regs.sp = make16bit(b1, b2)
 	}
 }
 
-func (regs *registers) getPair(reg string) uint16 {
-	switch reg {
+func (regs *registers) getPair(dest string) uint16 {
+	switch dest {
 	case "B":
-		return make16BitAddr(regs.b, regs.c)
+		return make16bit(regs.b, regs.c)
 	case "D":
-		return make16BitAddr(regs.d, regs.e)
+		return make16bit(regs.d, regs.e)
 	default:
+		if dest == "HL" || dest == "M" || dest == "H" {
+			return make16bit(regs.h, regs.l)
+		}
 		log.Fatal()
 	}
 	return 0
+}
+
+func (regs *registers) getReg(reg string) uint8 {
+	switch reg {
+	case "B":
+		return regs.b
+	case "C":
+		return regs.c
+	case "D":
+		return regs.d
+	case "E":
+		return regs.e
+	case "H":
+		return regs.h
+	case "L":
+		return regs.l
+	case "A":
+		return regs.a
+	}
+	return 0
+}
+
+func (regs *registers) updateReg(reg string, val uint8) {
+	switch reg {
+	case "B":
+		regs.b = val
+	case "C":
+		regs.c = val
+	case "D":
+		regs.d = val
+	case "E":
+		regs.e = val
+	case "H":
+		regs.h = val
+	case "L":
+		regs.l = val
+	case "A":
+		regs.a = val
+	}
 }
 
 func (cpu *cpu) readROM(n int) byte {
@@ -114,5 +157,107 @@ func (cpu *cpu) lxi() int {
 func (cpu *cpu) stax() int {
 	reg := cpu.opcode.operand
 	addr := cpu.regs.getPair(reg)
+	accumVal := cpu.regs.a
+	cpu.memory.write(addr, accumVal)
+	return 1
+}
 
+func (cpu *cpu) inx() int {
+	reg := cpu.opcode.operand
+	bits := cpu.regs.getPair(reg) + 1
+	b1 := uint8(bits >> 8)
+	b2 := uint8(bits & 0xff)
+	cpu.regs.writeRegs(reg, b1, b2)
+	return 1
+}
+
+func (cpu *cpu) inr() int {
+	reg := cpu.opcode.operand
+	var val uint8
+	if reg == "M" {
+		addr := cpu.regs.getPair(reg)
+		val = cpu.memory.read(addr)
+		val += 1
+		cpu.memory.write(addr, val)
+	} else {
+		val = cpu.regs.getReg(reg) + 1
+		cpu.regs.updateReg(reg, val)
+	}
+	cpu.flags.updateZSPAC(val)
+	return 1
+}
+
+func (cpu *cpu) dcr() int {
+	reg := cpu.opcode.operand
+	var val uint8
+	if reg == "M" {
+		addr := cpu.regs.getPair(reg)
+		val = cpu.memory.read(addr)
+		val -= 1
+		cpu.memory.write(addr, val)
+	} else {
+		val = cpu.regs.getReg(reg) - 1
+		cpu.regs.updateReg(reg, val)
+	}
+	cpu.flags.updateZSPAC(val)
+	return 1
+}
+
+func (cpu *cpu) mvi() int {
+	byte2 := cpu.romBuffer[cpu.regs.pc+1]
+	if cpu.opcode.operand == "M" {
+		addr := cpu.regs.getPair(cpu.opcode.operand)
+		cpu.memory.write(addr, byte2)
+	} else {
+		cpu.regs.updateReg(cpu.opcode.operand, byte2)
+	}
+	return 2
+}
+
+func (cpu *cpu) rlc() int {
+	reg := "A"
+	regVal := cpu.regs.getReg(reg)
+	prev7Bit := regVal & 0xf
+	regVal = regVal << 1
+	regVal = regVal | prev7Bit
+	cpu.regs.updateReg(reg, regVal)
+	cpu.flags.cy = prev7Bit
+	return 1
+}
+
+func (cpu *cpu) dad() int {
+	destReg := cpu.opcode.operand
+	hl := cpu.regs.getPair("HL")
+	value := cpu.regs.getPair(destReg)
+	sum := hl + value
+	if sum < 0xfff {
+		cpu.flags.cy = 1
+	} else {
+		cpu.flags.cy = 0
+	}
+	return 1
+}
+
+func (flags *flags) updateZSPAC(val uint8) {
+	if val == 0 {
+		flags.z = 1
+	} else {
+		flags.z = 0
+	}
+
+	if val == 128 {
+		flags.s = 1
+	} else {
+		flags.s = 0
+	}
+	flags.p = parity(val)
+}
+
+func parity(val uint8) uint8 {
+	count := bits.OnesCount8(val)
+	if count%2 == 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
--- memory.go
+++ memory.go
@@ -3,14 +3,14 @@ package main
 // 64KB RAM
 type memory [65536]byte
 
-func (mem *memory) write() {
-
+func (mem *memory) write(addr uint16, val uint8) {
+	mem[addr] = val
 }
 
-func (mem *memory) read() {
-
+func (mem *memory) read(addr uint16) uint8 {
+	return mem[addr]
 }
 
-func make16BitAddr(b1, b2 uint8) uint16 {
+func make16bit(b1, b2 uint8) uint16 {
 	return (uint16(b1) << 8) | uint16(b2)
 }
